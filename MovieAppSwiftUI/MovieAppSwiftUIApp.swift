4efa6de99a786a42aad082e1402bafed53448703
Nezih <nezihoksuzoglu@icloud.com>
2022-10-24 23:47:20 +0300

    Initial Commit

---
+++ MovieAppSwiftUI/MovieAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppSwiftUIApp.swift
+//  MovieAppSwiftUI
+//
+//  Created by Nezih on 24.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
